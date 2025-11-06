0506f9432d8f6c7de4ac4bbc8d3f9500558919a7
syihwamoza <mozakastella26@gmail.com>
2025-11-06 15:51:01 +0700

    Menambahkan field input 'Nama Lengkap' menggunakan OutlinedTextField

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -1,8 +1,16 @@
 package com.example.navigasiku.view
 
+import android.R.attr.text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormIsian(
@@ -17,7 +25,28 @@ fun FormIsian(
                 colors = TopAppBarDefaults.topAppBarColors
                     (colorResources(id = R.color.teal_700))
             )
-        })
+        }
+    ){ isiRuang ->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = {text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
+            HorizontalDivider(modifier = Modifier)
+        }
+    }
+}
+
+private fun ColumnScope.text(text: String) {
+    TODO("Not yet implemented")
 }
 
 @Composable
