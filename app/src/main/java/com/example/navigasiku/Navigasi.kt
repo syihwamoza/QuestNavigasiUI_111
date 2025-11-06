3597645352ace17eb762b1cb8549a3b80d19c153
syihwamoza <mozakastella26@gmail.com>
2025-11-06 17:48:55 +0700

    commit 2 kali

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -1,2 +1,58 @@
 package com.example.navigasiku
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.example.navigasiku.view.FormIsian
+import com.example.navigasiku.view.TampilData
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navControlller: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier,
+) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navControlller,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Formulirku.name) {
+                FormIsian(
+                    //pilihanJK = JenisK.map { id -> konteks.resources.getString(id)}
+                    OnSubmitBtnClick = {
+                        navControlller.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                TampilData(
+                    onBackClick = {
+                        cancelAndBackToFormulirku(navControlller)
+                    }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulirku(
+    navController: NavHostController
+) {
+    navController.popBackStack(
+        Navigasi.Formulirku.name,
+        inclusive = false
+    )
+}
--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -1,74 +1,109 @@
 package com.example.navigasiku.view
 
-import android.R.attr.text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.res.colorResource
+import com.example.navigasiku.R
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
-    jenisK:List<String> = ListOf("Laki-laki", "Perempuan"),
-    OnSubmitBtnClick : () -> Unit
-){
-    Scaffold (modifier= Modifier,
-        topBar={
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick: () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
             TopAppBar(
-                title = {Text (stringResources(id= R.string.home),
-                    color = Color.White)},
+                title = {
+                    Text(
+                        stringResource(id = R.string.home),
+                        color = Color.White
+                    )
+                },
                 colors = TopAppBarDefaults.topAppBarColors
-                    (colorResources(id = R.color.teal_700))
+                    (colorResource(id = R.color.teal_700))
             )
         }
-    ){ isiRuang ->
-        Column(modifier = Modifier.padding(isiRuang),
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
+        ) {
             OutlinedTextField(
                 value = "",
                 singleLine = true,
                 modifier = Modifier
                     .padding(top = 20.dp)
                     .width(250.dp),
-                label = {text(text = "Nama Lengkap")},
+                label = { Text(text = "Nama Lengkap") },
                 onValueChange = {},
             )
-            HorizontalDivider(modifier = Modifier
-                .padding(20.dp)
-                .width(250.dp), thickness = Thickness,color =
-            Color.Red)
-            Row{
-                jenisK.forEach{
-                    item->
-                    Row(verticalAlignment = Alignment.CenterVertically){
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .width(250.dp), thickness = Thickness, color =
+                    Color.Red
+            )
+            Row {
+                jenisK.forEach { item ->
+                    Row(verticalAlignment = Alignment.CenterVertically) {
                         RadioButton(
                             selected = false,
-                            onClick = {item}
+                            onClick = { item }
                         )
+                        Text(text = item)
                     }
                 }
+            }
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(250.dp),
+                label = { Text(text = "Alamat") },
+                onValueChange = {},
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(
+                ),
+                onClick = OnSubmitBtnClick
+            ) {
+                Text(stringResource(id = R.string.submit))
             }
         }
     }
 }
 
-private fun ColumnScope.text(text: String) {
-    TODO("Not yet implemented")
-}
-
-@Composable
-fun ListOf(x0: String, x1: String) {
-    TODO("Not yet implemented")
-}
