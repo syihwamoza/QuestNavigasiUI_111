670ecdeb72f86bfe1aebf6a0a8fa16f1f8e06825
syihwamoza <mozakastella26@gmail.com>
2025-11-06 15:39:54 +0700

    menambahkan FormIsian dengan TopAppBar dan list jenis kelamin

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -1,2 +1,26 @@
 package com.example.navigasiku.view
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormIsian(
+    jenisK:List<String> = ListOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+    Scaffold (modifier= Modifier,
+        topBar={
+            TopAppBar(
+                title = {Text (stringResources(id= R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors
+                    (colorResources(id = R.color.teal_700))
+            )
+        })
+}
+
+@Composable
+fun ListOf(x0: String, x1: String) {
+    TODO("Not yet implemented")
+}
