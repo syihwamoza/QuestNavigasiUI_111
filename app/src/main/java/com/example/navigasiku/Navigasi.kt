d5ad1fdb17a80b8ef0cfb6c5f27017c42865892a
syihwamoza <mozakastella26@gmail.com>
2025-11-06 18:01:50 +0700

    feat: tambah fungsi cancelAndBackToFormulirku untuk tombol kembali

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -47,4 +47,13 @@ fun DataApp(
         }
     }
 }
+private fun cancelAndBackToFormulirku(
+    navController: NavHostController
+) {
+    navController.popBackStack(
+        Navigasi.Formulirku.name,
+        inclusive = false
+    )
+}
+
 
