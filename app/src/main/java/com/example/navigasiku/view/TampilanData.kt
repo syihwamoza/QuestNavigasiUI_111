70f43bec384d869212ae16cb279462f5a221fd9a
syihwamoza <mozakastella26@gmail.com>
2025-11-06 17:52:53 +0700

    mulai pembuatan form input pengguna

--- app/src/main/java/com/example/navigasiku/view/TampilanData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilanData.kt
@@ -2,88 +2,35 @@ package com.example.navigasiku.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import com.example.navigasiku.R
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.res.colorResource
+import com.example.navigasiku.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TampilData(
-    onBackClick: () -> Unit
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick: () -> Unit
 ) {
-    val items = listOf(
-        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
-        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
-        Pair(stringResource(id = R.string.alamat),"Yogyakarta"),
-    )
-    Scaffold(
-        modifier = Modifier,
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = stringResource(id = R.string.tampil),
-                        color = Color.White
-                    )
-                },
-                colors = TopAppBarDefaults
-                    .mediumTopAppBarColors(colorResource
-                    (id = R.color.teal_700)
-            ))
-        }) { isiRuang ->
-        Column(
-            modifier = Modifier.padding(isiRuang),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            Column(
-                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.spacedBy(
-                    dimensionResource
-                        (id = R.dimen.padding_small)
-                )
-            ) {
-                items.forEach { item ->
-                    Column {
-                        Text(
-                            text = item.first.uppercase(),
-                            fontSize = 16.sp
-                        )
-                        Text(
-                            text = item.second,
-                            fontWeight = FontWeight.Bold,
-                            fontFamily = FontFamily.Cursive, fontSize = 22.sp
-                        )
-                    }
-                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
-                }
-                Spacer(modifier = Modifier.height(10.dp))
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = onBackClick
-                ) {
-                    Text(text = stringResource(id = R.string.back))
-                }
-            }
-        }
-    }
-}
